45e4016ab788b683d326a32b5fab257199952226
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-02-04 12:52:52 -0600

    React.createElement vs JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement('a',{ href:'https://www.platzi.com', target:"_blank"},"Ir a platzi");
 
+const name = 'Irene';
+// const element = React.createElement('h1',{},`Hola soy ${name}`);
+// const element = <h1> Hola soy { name } !!</h1>
+
+const jsx = (
+    <div>
+        <h1>Hola, soy { name }</h1>
+        <p>Soy ingeniero frontend</p>
+    </div>
+)
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
